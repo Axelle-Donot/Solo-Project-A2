fe58bef03967fc75a90063e7806336dc45fe6c88
Rotashin <mateo.fernandez@etu.umontpellier.fr>
2021-11-21 15:10:54 +0100

    .\proj__product.sql

--- scripts-sql/proj__product.sql
+++ scripts-sql/proj__product.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost
--- Généré le :  jeu. 18 nov. 2021 à 11:18
--- Version du serveur :  5.5.47-0+deb8u1
--- Version de PHP :  7.2.22-1+0~20190902.26+debian8~1.gbpd64eb7
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 21 nov. 2021 à 15:10
+-- Version du serveur : 10.4.19-MariaDB
+-- Version de PHP : 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,19 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `danom`
+-- Base de données : `solo`
 --
 
 --
 -- Déchargement des données de la table `proj__product`
 --
 
-INSERT INTO `proj__product` (`product_id`, `tag_id`, `discount_id`, `price`, `rating`) VALUES
-(1, 1, 1, 159.99, NULL),
-(2, 2, NULL, 99.99, NULL),
-(3, 3, NULL, 24.99, NULL),
-(4, 4, 3, 479.99, NULL),
-(5, 1, 2, 159.99, NULL);
+INSERT INTO `proj__product` (`product_id`, `tag_id`, `discount_id`, `name`, `description`, `product_picture_id`, `price`, `rating`) VALUES
+(2, 1, NULL, 'sabre1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam purus, eleifend vel leo in, pharetra placerat sem. Nam dictum magna semper, semper diam in, tempus ligula. Proin id porta enim. Sed tincidunt tellus ac massa malesuada sagittis. Phasellus r', NULL, 68.99, NULL),
+(3, 1, 2, 'sabre2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam purus, eleifend vel leo in, pharetra placerat sem. Nam dictum magna semper, semper diam in, tempus ligula. Proin id porta enim. Sed tincidunt tellus ac massa malesuada sagittis. Phasellus r', NULL, 19.99, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
