b3dff8f9ced6b1b10424246c872f6c087c90e7cb
Axelle Donot <axelle.donot@etu.umontpellier.fr>
2021-11-28 11:18:32 +0100

    modification dans ControllerCart_item mais qui n'ont pas résolu le problème 😭

--- app/controllers/ControllerCart_item.php
+++ app/controllers/ControllerCart_item.php
@@ -5,18 +5,11 @@ class ControllerCart_item {
   protected static $object = "cart_item";
   
   public static function ajout(){
+    $prod = ModelCart_item::ajoutProduitPanier($_GET['id']);
 
-    $prod = ModelProduct::ajoutProduitPanier($_GET['id']);
-
-    if ($value){
-      $page_title = 'Ajout du produit';
-      $view = 'ajoutProduct';
-      require(File::getApp(array("views", "view.php")));
-    }else{
-      $page_title = 'Error';
-      $view = 'error';
-      require(File::getApp(array("views", "view.php")));
-    }
+    $page_title = 'Ajout du produit';
+    $view = 'ajoutProduct';
+    require(File::getApp(array("views", "view.php")));
   }
 
 
