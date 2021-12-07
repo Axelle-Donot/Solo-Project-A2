fb06b371a7074a8b54cbaa4fe281fe0fb3ea2bd5
Rotashin <mateo.fernandez@etu.umontpellier.fr>
2021-12-08 00:58:58 +0100

    ouais nan c'est bizarre après on peut pas les appeler dans un contexte static

--- app/models/ModelImages.php
+++ app/models/ModelImages.php
@@ -8,8 +8,8 @@ class ModelImages extends Model {
   private $img_type;
   private $img_desc;
   private $img_blob;
-  private $default_image = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAIVBMVEXMzMyWlpbFxcWjo6O+vr63t7ecnJyqqqqbm5uxsbGampoKZyAaAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAU0lEQVRIiWNgGAWjYBQMd8Bk7KCITGMAZqeAQgZ2zwAwjQ2wmYU6ArVDaGyA0aWTkYHBFEpjM0FpoaABUzGExqaAJdmBkYFdNQBMj4JRMAoGOwAAPNIL2qWeApgAAAAASUVORK5CYII=";
-  private $default_type = "png";
+  private static $default_image = "iVBORw0KGgoAAAANSUhEUgAAAEAAAABABAMAAABYR2ztAAAAIVBMVEXMzMyWlpbFxcWjo6O+vr63t7ecnJyqqqqbm5uxsbGampoKZyAaAAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAU0lEQVRIiWNgGAWjYBQMd8Bk7KCITGMAZqeAQgZ2zwAwjQ2wmYU6ArVDaGyA0aWTkYHBFEpjM0FpoaABUzGExqaAJdmBkYFdNQBMj4JRMAoGOwAAPNIL2qWeApgAAAAASUVORK5CYII=";
+  private static $default_type = "png";
 
   protected static $object = "images";
   protected static $primary = "img_id";
