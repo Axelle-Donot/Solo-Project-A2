ebcb1dfd063138b8fb24549901236c3a45467f41
Axelle Donot <axelle.donot@etu.umontpellier.fr>
2021-12-14 08:32:24 +0100

    blob marche toujours pas

--- app/controllers/ControllerImages.php
+++ app/controllers/ControllerImages.php
@@ -4,7 +4,7 @@ require_once File::getApp(array("models", "ModelImages.php"));
 class ControllerImages {
   protected static $object = "images";
 
-  public static function addimg($value) {
-    ModelUser::create($value);
+  public static function addImg($value) {
+    ModelImages::create($value);
   }
 }
